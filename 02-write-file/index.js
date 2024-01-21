0e46fcc1462257e2856dbb1259dcbf1393918698
Dzmitry Drevich <jeidan2012@gmail.com>
2024-01-21 23:49:26 +0100

    feat: add solution to task 02

---
+++ 02-write-file/index.js
@@ -0,0 +1,15 @@
+const { stdin, stdout, exit } = require('process');
+const fs = require('fs');
+const path = require('path');
+
+stdout.write('Greetings, fellow!\n');
+const output = fs.createWriteStream(path.join(__dirname, 'output.txt'));
+stdin.on('data', (data) => {
+  let text = data.toString().trim();
+  if (text.toLowerCase() !== 'exit') {
+    output.write(`${text}\n`);
+  } else {
+    stdout.write('Goodbuy, fellow!\n');
+    exit();
+  }
+});
