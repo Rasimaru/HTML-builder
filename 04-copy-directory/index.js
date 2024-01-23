7312985f875dd3fee0657704a4cfddd2236ca743
Dzmitry Drevich <jeidan2012@gmail.com>
2024-01-23 14:37:17 +0100

    fix: add fix for "changing-copy"

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,6 +5,25 @@ const fsPromises = require('fs').promises;
 const src = path.join(__dirname, 'files/');
 const dest = path.join(__dirname, 'files-copy');
 
+fs.readdir(dest, { withFileTypes: true }, (err, files) => {
+  if (err) {
+    // console.log('nope');
+  }
+  if (files) {
+    files.forEach((file) => {
+      fs.rm(
+        path.join(dest, file.name),
+        { recursive: true, force: true },
+        (err) => {
+          if (err) {
+            throw err;
+          }
+        },
+      );
+    });
+  }
+});
+
 fs.mkdir(dest, { recursive: true }, () => {
   console.log('Directory created successfully');
   fs.readdir(src, { withFileTypes: true }, function (err, files) {
